8ddb55c77aba824120bb96d9d8b353d9b21c1642
Rickey Shideler <rdshideler@gmail.com>
2022-09-29 19:50:01 +0000

    added input validation to setup

--- setup.py
+++ setup.py
@@ -3,19 +3,27 @@ import shutil
 from pathlib import Path
 
 if __name__ == "__main__":
+
+    MIN_YEAR = 2015
+    MAX_YEAR = 2022
+
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--year', type=int, required=True)
-    parser.add_argument('--day', type=int, required=True)
+    parser.add_argument("year", type=int)
+    parser.add_argument("day", type=int)
     args = parser.parse_args()
 
+    if not (MIN_YEAR <= args.year <= MAX_YEAR):
+        raise argparse.ArgumentTypeError("invalid year")
+
+    if not (1 <= args.day <= 31):
+        raise argparse.ArgumentTypeError("invalid day")
+
     base_path = Path(__file__).parent.absolute()
-    template_dir = base_path / 'template'
+    template_dir = base_path / "template"
     day_dir = base_path / f"{args.year}" / f"{args.day}"
 
     Path(day_dir).mkdir(parents=True, exist_ok=True)
 
     for file_path in template_dir.iterdir():
         shutil.copy(file_path, day_dir / file_path.name)
-
-
--- template/main.py
+++ template/main.py
@@ -1,15 +1,18 @@
 import os
 
-def solution1():
+
+def solution1(data):
     return None
 
-def solution2():
+
+def solution2(data):
     return None
+
 
 if __name__ == "__main__":
     dir = os.path.abspath(os.path.dirname(__file__))
     with open(os.path.join(dir, "input.txt")) as file:
         data = file.read().splitlines()
 
-    print(solution1())
-    print(solution2())
+    print(solution1(data))
+    print(solution2(data))
